f901fb44693dc75b59e30176e9751d6db6ce8580
Felipe Borreli <felipeborreli@gmail.com>
2022-02-11 01:54:54 -0300

    refactor: add customized error for missing param

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,18 @@
 module.exports = {
+  preset: 'ts-jest',
+  globals: {
+    'ts-jest': {
+      isolatedModules: true
+    }
+  },
   roots: ['<rootDir>/src'],
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
+  },
+  moduleNameMapper: {
+    '@/(.*)': '<rootDir>/src/$1'
   }
 }
