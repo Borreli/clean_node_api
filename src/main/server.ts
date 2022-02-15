12e00ff9b154f9389a6aed40dd4213a47374cb4f
Felipe Borreli <felipeborreli@gmail.com>
2022-02-15 17:01:23 -0300

    chore: remove sucrase

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,4 @@
 import app from '@/main/config/app'
 
 const port = 5050
-app.listen(port, () => console.log(`Server running at http:localhost: ${port}`))
+app.listen(port, () => console.log(`Server running at http:localhost:${port}`))
