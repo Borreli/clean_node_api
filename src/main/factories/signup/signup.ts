05cfcd3c441cd1fcce9eebf9fe2da5381316de2f
Felipe Borreli <felipeborreli@gmail.com>
2022-02-19 21:15:24 -0300

    feat: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

--- src/main/factories/signup/signup.ts
+++ src/main/factories/signup/signup.ts
@@ -1,4 +1,4 @@
-import { DbAddAccount } from '@/data/usecases/add-account/db-add-account'
+import { DbAddAccount } from '@/data/usecases'
 import { BcryptAdapter } from '@/infra/cryptography/bcrypt-adapter'
 import { AccountMongoRepository, LogMongoRepository } from '@/infra/db/mongodb'
 import { LogControllerDecorator } from '@/main/decorators'
