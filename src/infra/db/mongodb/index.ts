235ec0faa56e309ba7d9ffc7087beb227484ae65
Felipe Borreli <felipeborreli@gmail.com>
2022-03-14 03:15:21 -0300

    feat: ensure SurveyResultMongoRepository insert new surveyResult if its new

--- src/infra/db/mongodb/index.ts
+++ src/infra/db/mongodb/index.ts
@@ -1,3 +1,4 @@
 export * from './account/account-mongo-repository'
 export * from './log/log-mongo-repository'
 export * from './survey/survey-mongo-repository'
+export * from './survey-result/survey-result-mongo-repository'
