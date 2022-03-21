4ba6123cafb6092a4a5c36d5f5463abcae976b81
Felipe Borreli <felipeborreli@gmail.com>
2022-03-21 05:25:52 -0300

    docs: add login api to swagger

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,24 @@
+import { loginPath } from '@/main/docs/paths'
+import { accountSchema, loginParamsSchema } from '@/main/docs/schemas'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'API do curso do Mango para realizar enquetes entre programadores',
-    version: '1.5.1'
+    version: '0.5.1'
+  },
+  servers: [{
+    url: '/api'
+  }],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
